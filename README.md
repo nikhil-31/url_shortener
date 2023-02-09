a0a33f7898f55dbdf63c5214df0a18be7a15f53f
nikhil-31 <nkl07ba@gmail.com>
2023-02-09 16:19:02 +0530

    Added forward, delete URL support

--- README.md
+++ README.md
@@ -1,10 +1,55 @@
 ## URL shortener using FAST API
 
+### Overview
+Fast API implementation of a URL shortener,
+Features 
+1. Create a shortened URL
+2. Forward to target URL using the shortened URL
+3. Admin info - clicks on the shortened URL
+4. Delete/ deactivate - Deactivate the shortened URL
+
+
+### Getting started
+
+1. Clone the project and change directory into the cloned folder
+2. Create a virtual environment (venv) with python version above 3.6.5 
+3. Activate the virtual environment and install the dependencies in the 
+   requirements.txt
+4. The sqlite database will be automatically created when the 
+   application is started.
+5. The sample of the environment variables file `.env.sample` is present 
+   in the root directory, rename\copy this file to be `.env`
+   The required env variables
+   ```
+   ENV_NAME="Replace with name of the environment eg: dev/local/prod"
+   BASE_URL="Replace with the base URL eg: localhost:8000, domain_name"
+   DB_URL="Replace with path to the db eg:sqlite:///./shortener.db"
+   ```
+6. Uvicorn is used as the application server, to start the application
+   ```
+   uvicorn shortener_app.main:app --reload
+   ```
+   
+### Libraries used
+- FastAPI
+- SQLAlchemy
+- uvicorn
+- validators
+
 ### Endpoints
 
-| Endpoint | HTTP Verb | Request body | Action |
-| / | GET |  | Returns a static string output |
-| /url | POST | Your URL target | Created url_key with additional info using secret key |
-| /{url_key} | GET |  | Forwards to your target URL |
-| /admin/{secret_key} | GET | Request body | Shows admin info about your shortened URL |
-| /admin/{secret_key} | DELETE | Your secret key | Deletes your shortened URL |
+| Endpoint            | HTTP Verb | Request body    | Action                                                |
+|---------------------|-----------|-----------------|-------------------------------------------------------|
+| /                   | GET       |                 | Returns a static string output                        |
+| /url                | POST      | Your URL target | Created url_key with additional info using secret key |
+| /{url_key}          | GET       |                 | Forwards to your target URL                           |
+| /admin/{secret_key} | GET       | Request body    | Shows admin info about your shortened URL             |
+| /admin/{secret_key} | DELETE    | Your secret key | Deletes your shortened URL                            |
+
+
+### License
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+Copyright 2023 Nikhil Bhaskar
--- shortner_app/main.py
+++ shortner_app/main.py
@@ -1,10 +1,13 @@
-import secrets
 import validators
-from .schemas import URL, URLBase, URLInfo
-from . import models
-from fastapi import FastAPI, HTTPException, Depends
+
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
+
+from . import models, schemas, crud
 from .database import SessionLocal, engine
+from .config import get_settings
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -18,8 +21,23 @@ def get_db():
         db.close()
 
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
+
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist."
+    raise HTTPException(status_code=404, detail=message)
 
 
 @app.get("/")
@@ -27,23 +45,45 @@ def read_root():
     return "Welcome to the URL shortener app"
 
 
-@app.post("/url", response_model=URLInfo)
-def create_url(url: URLBase):
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
 
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key=secret_key
-    )
-    # db.add(db_url)
-    # db.commit()
-    # db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
-
-    return f"TODO: Create database entry for: {url.target_url}"
+    db_url = crud.create_db_url(db=db, url=url)
+    return get_admin_info(db_url)
 
 
+@app.get("/{url_key}")
+def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        crud.update_db_clicks(db=db, db_url=db_url)
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.get("/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo)
+def get_url_info(
+        secret_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db=db, secret_key=secret_key):
+        return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
+
+
+@app.delete("/admin/{secret_key}")
+def delete_url(
+        secret_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+):
+    if db_url := crud.deactivate_db_url_by_secret_key(db, secret_key):
+        message = (f"Successfully deleted shortened URL for "
+                   f"{db_url.target_url}")
+        return {"detail": "message"}
+    else:
+        raise_not_found(request)
